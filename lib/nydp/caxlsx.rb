34a1088ad309f2d36d3e9b5cca58a3e80b24c1d0
conanite <conan@conandalton.net>
2021-08-02 17:18:46 +0200

    nydp: use #builtin_call instead of #call in order to take advantage of nydp error handling

--- lib/nydp/caxlsx.rb
+++ lib/nydp/caxlsx.rb
@@ -36,50 +36,39 @@ module Nydp
     module Builtin
       class NewPackage
         include Nydp::Builtin::Base, Singleton
-        def call
-          # vm.push_arg Axlsx::Package.new
+        def builtin_call
           Axlsx::Package.new
         end
       end
 
       class NewWorksheet
         include Nydp::Builtin::Base, Singleton
-        # def builtin_invoke_3 vm, package, name
-        def call package, name
-          # vm.push_arg package.workbook.add_worksheet(name.is_a?(Hash) ? rubify(name) :  { name: rubify(name) })
+        def builtin_call package, name
           package.workbook.add_worksheet(name.is_a?(Hash) ? rubify(name) :  { name: rubify(name) })
         end
       end
 
       class NewRow
         include Nydp::Builtin::Base, Singleton
-        # def builtin_invoke_3 vm, sheet, values
-        def call sheet, values
-          # vm.push_arg sheet.add_row(rubify(values))
+        def builtin_call sheet, values
           sheet.add_row(rubify(values))
         end
 
-        # def builtin_invoke_4 vm, sheet, values, options
-        def call sheet, values, options={}
-          # vm.push_arg sheet.add_row(rubify(values), rubify(options))
+        def builtin_call sheet, values, options={}
           sheet.add_row(rubify(values), rubify(options))
         end
       end
 
       class SetColumnWidths
         include Nydp::Builtin::Base, Singleton
-        # def builtin_invoke_3 vm, sheet, values
-        def call sheet, values
-          # vm.push_arg sheet.column_widths(rubify(values))
+        def builtin_call sheet, values
           sheet.column_widths(rubify(values))
         end
       end
 
       class NewStyle
         include Nydp::Builtin::Base, Singleton
-        # def builtin_invoke_3 vm, package, rules
-        def call package, rules
-          # vm.push_arg package.workbook.styles.add_style(rubify(rules))
+        def builtin_call package, rules
           package.workbook.styles.add_style(rubify(rules))
         end
       end
